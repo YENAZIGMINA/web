fb7377d0473dd25d8fef42c37102bc230e2b5e6a
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-04-28 17:44:53 +0900

    Add files via upload

--- web/web standard/js/custom.js
+++ web/web standard/js/custom.js
@@ -15,9 +15,45 @@ $(document).ready(function () {
         slidesToShow: 3, // 첫화면에 3개보이고
         slidesToScroll: 1,  // 한칸씩 움직일지? 3개씩 움직일지 지정
         
-        dots: true, // 밑에 점
+        dots: true, // 밑에 점bar
 
         autoplay:true,
         autoplaySpeed: 2000
     });
 });
+
+//탭메뉴
+let tab_list=$('.tab_menu');
+
+tab_list.find('ul ul').hide();
+tab_list.find('li.active ul').show();
+
+$('.tab_menu>ul>li>a').click(function(event){
+    event.preventDefault();
+    
+    var $this = $(this);
+
+    //밑에꺼 한줄로 줄이기
+     $this.next().show().parent('li').addClass("active").siblings('li').removeClass("active").find('ul').hide();
+
+    
+/*  $this.next().show();
+    $this.parent('li').addClass("active"); 
+    $this.parent('li').siblings('li').removeClass("active");
+    $this.parent('li').siblings('li').find('ul').hide(); */
+    
+
+/*  🔹.next()는 바로 아래 동생 의 ul을 show해라
+    $this.next().show();
+
+    🔹.parent()는 클릭한 것의 부모 에게 클래스명 "active"를 추가해라 
+      (css에서 active에 작성한 값 실행됨)
+    $this.parent('li').addClass("active");
+
+    🔹.siblings()는 나를 제외한 형제 들은 클래스명을 빼라
+    $this.parent('li').siblings('li').removeClass("active");
+    
+    🔹 내 부모li의 다른형제li들의 히위요소(find)인 ul은 숨겨라
+    $this.parent('li').siblings('li').find('ul').hide(); */
+
+})
